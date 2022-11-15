d3f2691514fc2af50dd70398410cd9d8f0e5fbd9
edwardmm03 <edwardmm@Clarkson.edu>
2022-11-14 19:20:51 -0500

    ClackData now implements Serializable

--- src/data/ClackData.java
+++ src/data/ClackData.java
@@ -1,8 +1,9 @@
 package data;
 
+import java.io.Serializable;
 import java.util.Date;
 
-public abstract class ClackData
+public abstract class ClackData implements Serializable
 {
     //private variables
     private String username;
